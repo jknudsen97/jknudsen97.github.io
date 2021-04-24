d5da3b2b037af7c27c849ceee5b3b90e195f211a
jknudsen97 <jeremythenome@gmail.com>
2021-04-23 19:13:15 -0600

    commit

---
+++ W01/script.js
@@ -0,0 +1,6 @@
+let date = new Date();
+let x = date.getFullYear();
+
+document.querySelector('#currentYear').textContent = x;
+
+document.querySelector('#lastMod').textContent = document.lastModified;
