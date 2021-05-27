df18e8507407aa23288bc951298adf6f2618c2af
Jeremy Knudsen <jeremythenome@gmail.com>
2021-05-27 17:58:59 -0600

    commit

---
+++ .history/lesson05/js/script_20210527175651.js
@@ -0,0 +1,87 @@
+const hamburger = document.querySelector(".hamburger");
+const menu = document.querySelector(".menu");
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    menu.classList.toggle("active");
+}
+
+const link = document.querySelectorAll(".link");
+link.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu(){
+    hamburger.classList.remove("active");
+    menu.classList.remove("active");
+}
+
+var d = new Date();
+var weekday = new Array(7);
+weekday[0] = "Sunday";
+weekday[1] = "Monday";
+weekday[2] = "Tuesday";
+weekday[3] = "Wednesday";
+weekday[4] = "Thursday";
+weekday[5] = "Friday";
+weekday[6] = "Saturday";
+
+var n = weekday[d.getDay()];
+document.querySelector("#currentDayofWeek").innerHTML = n;
+
+var n = d.getDate();
+document.querySelector("#currentDay").innerHTML = n;
+
+var month = new Array();
+month[0] = "January";
+month[1] = "February";
+month[2] = "March";
+month[3] = "April";
+month[4] = "May";
+month[5] = "June";
+month[6] = "July";
+month[7] = "August";
+month[8] = "September";
+month[9] = "October";
+month[10] = "November";
+month[11] = "December";
+
+var n = month[d.getMonth()];
+document.querySelector("#currentMonth").innerHTML = n;
+
+var n = d.getFullYear();
+document.querySelector("#currentYear").innerHTML = n;
+
+//Lesson05
+
+/*var header = document.querySelector("#nav");
+var btns = header.getElementsByClassName("link");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+  var current = document.getElementsByClassName("active");
+  if (current.length > 0) { 
+    current[0].className = current[0].className.replace(" active", "");
+  }
+  this.className += " active";
+  });
+}*/
+
+const current = 0;
+for (var i = 0; i < document.links.length; i++) {
+    if (document.links[i].href === document.URL) {
+        current = i;
+    }
+}
+document.links[current].className = 'current';
+
+let thedate = new Date();
+const friday = thedate.getDay();
+const banner = document.querySelector('.banner');
+
+if (thedate.getDay() == 4)
+{
+  document.querySelector('.banner').style.display = 'block';
+}
+else
+{
+  banner.style.display = "none";
+}
