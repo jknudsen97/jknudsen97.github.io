07d3918205e24cd5db19691f83ac7d1b0d3ede30
Jeremy Knudsen <jeremythenome@gmail.com>
2021-06-05 16:51:53 -0600

    commit

---
+++ .history/lesson07/js/script_20210604212647.js
@@ -0,0 +1,93 @@
+const hamburger = document.querySelector(".hamburger");
+const menu = document.querySelector(".menu");
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    menu.classList.toggle("active");
+}
+
+const link = document.querySelectorAll(".link");
+link.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu(){
+    hamburger.classList.remove("active");
+    menu.classList.remove("active");
+}
+
+var d = new Date();
+var weekday = new Array(7);
+weekday[0] = "Sunday";
+weekday[1] = "Monday";
+weekday[2] = "Tuesday";
+weekday[3] = "Wednesday";
+weekday[4] = "Thursday";
+weekday[5] = "Friday";
+weekday[6] = "Saturday";
+
+var n = weekday[d.getDay()];
+document.querySelector("#currentDayofWeek").innerHTML = n;
+
+var n = d.getDate();
+document.querySelector("#currentDay").innerHTML = n;
+
+var month = new Array();
+month[0] = "January";
+month[1] = "February";
+month[2] = "March";
+month[3] = "April";
+month[4] = "May";
+month[5] = "June";
+month[6] = "July";
+month[7] = "August";
+month[8] = "September";
+month[9] = "October";
+month[10] = "November";
+month[11] = "December";
+
+var n = month[d.getMonth()];
+document.querySelector("#currentMonth").innerHTML = n;
+
+var n = d.getFullYear();
+document.querySelector("#currentYear").innerHTML = n;
+
+
+const imagesToLoad = document.querySelectorAll('img[data-src]');
+
+const loadImages = (image) => {
+    image.setAttribute('src', image.getAttribute('data-src'));
+    image.onload = () => {
+        image.removeAttribute('data-src');
+    };
+};
+
+const imgOptions = {
+    rootMargin: '0px 0px 50px 0px',
+    threshold: 1
+};
+
+if ('IntersectionObserver' in window) {
+    const imgObserver = new IntersectionObserver(items => {
+        items.forEach(item => {
+            if (item.isIntersecting) {
+                loadImages(item.target);
+                imgObserver.unobserve(item.target);
+            }
+        });
+    }, imgOptions);
+
+    imagesToLoad.forEach((img) => {
+        imgObserver.observe(img);
+    });
+} else {
+    imagesToLoad.forEach(img => {
+        loadImages(img);
+    });
+}
+
+var date_diff = function(date1, date2) {
+    var local = window.localStorage.getItem("dt1");
+    dt1 = new Date(date1);
+    dt2 = new Date(date2);
+    return Math.floor((Date.UTC(dt2.now()) - Date.UTC(dt1.getDate())));
+}
