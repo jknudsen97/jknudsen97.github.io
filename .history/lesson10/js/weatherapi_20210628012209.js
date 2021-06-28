33e483fc56086b5b9654026275e1b0ef1cdffcb3
Jeremy Knudsen <jeremythenome@gmail.com>
2021-06-28 01:23:25 -0600

    commit

---
+++ .history/lesson10/js/weatherapi_20210628012209.js
@@ -0,0 +1,18 @@
+let cityID = 5604473
+let appid = '4288c843ab0418aa9169c7a83367910c';
+const apiURL = `https://api.openweathermap.org/data/2.5/forecast?id=${cityID}&appid=${appid}`;
+
+fetch(apiURL)
+.then((response) => response.json())
+.then((jsObject) => {
+    const temperature = document.querySelector('#temp');
+    temperature.textContent = jsObject.main.temp.toFixed(0);
+    const currently = document.querySelector('#current');
+    const desc = jsObject.weather[0].description;
+    currently.innerHTML = `<strong>${desc.toUpperCase()}</strong>`;
+
+    const imagesrc = `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
+    document.getElementById('imagesrc').textContent = imagesrc;
+    document.getElementById('icon').setAttribute('src', imagesrc);
+    document.getElementById('icon').setAttribute('alt', desc.toUpperCase());
+});
