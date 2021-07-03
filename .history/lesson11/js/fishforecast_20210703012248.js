65c1eb62bc20807a3975835fd3c40c84bcb020e8
Jeremy Knudsen <jeremythenome@gmail.com>
2021-07-03 01:24:17 -0600

    commit

---
+++ .history/lesson11/js/fishforecast_20210703012248.js
@@ -0,0 +1,26 @@
+let cityID = '5585010';
+let appid = '4288c843ab0418aa9169c7a83367910c';
+const apiURL = `https://api.openweathermap.org/data/2.5/forecast?id=${cityID}&appid=${appid}&units=imperial`;
+
+fetch(apiURL)
+.then((response) => response.json())
+.then((jsObject) => {
+    console.log(jsObject);
+
+    let day = 0;
+    const dayofWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+    console.log(new Date(d.dt_txt));
+
+    const fiveDayForecast = jsObject.list.filter(forcast => forcast.dt_txt.includes('12:00:00'));
+    console.log(fiveDayForecast);
+
+    fiveDayForecast.forEach(x => {
+        console.log(x.main.temp);
+        console.log(dayofWeek[day + 1]);
+        let d = x.dt_txt;
+        console.log(d);
+        document.getElementById(`dayofweek${day + 1}`).textContent = dayofWeek[day + 1];
+        document.getElementById(`forecast${day + 1}`).textContent = Math.round(x.main.temp);
+        day++
+    })
+})
---
+++ lesson11/js/fishweatherapi.js
@@ -0,0 +1,43 @@
+let cityid = '5585010';
+let appID = '4288c843ab0418aa9169c7a83367910c';
+const apiurl = `https://api.openweathermap.org/data/2.5/weather?id=${cityid}&appid=${appID}&units=imperial`;
+
+fetch(apiurl)
+.then((response) => response.json())
+.then((jsObject) => {
+    const temp = document.querySelector('#temp');
+    temp.textContent = jsObject.main.temp.toFixed(0);
+    console.log(jsObject.main.temp.toFixed(0));
+    const high = document.querySelector('#high');
+    high.textContent = jsObject.main.temp_max.toFixed(0);
+    const humidity = document.querySelector('#humidity');
+    humidity.textContent = jsObject.main.humidity.toFixed(0);
+    const speed = document.querySelector('#speed');
+    speed.textContent = jsObject.wind.speed.toFixed(0);
+    const desc = jsObject.weather[0].description;
+    currently.innerHTML = desc;
+    console.log(desc);
+
+    const imagesrc = `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
+    document.getElementById('imagesrc').textContent = imagesrc;
+    document.getElementById('icon').setAttribute('src', imagesrc);
+    document.getElementById('icon').setAttribute('alt', desc.toUpperCase());
+});
+
+function windChill(temp, speed)
+{
+    let wChill = 35.74 + (0.6215 * temp) - (35.75 * Math.pow(speed, 0.16)) + (0.4275 * temp) * (Math.pow(speed, 0.16));
+    return Math.round(wChill);
+}
+
+if (temp <= 50 && speed >= 3)
+{
+    let wc = windChill(temp, speed);
+    document.querySelector('#WC').textContent = wc;
+}
+
+else
+{
+    let wc = "N/A";
+    document.querySelector("#WC").textContent = wc;
+}
