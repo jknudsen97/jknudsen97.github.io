dbdd897a7894e1c400ac791826161ffdec406849
Jeremy Knudsen <jeremythenome@gmail.com>
2021-07-21 18:19:19 -0600

    commit

---
+++ .history/finalproject/js/script_20210721154137.js
@@ -0,0 +1,141 @@
+const hamburger = document.querySelector(".hamburger");
+const menu = document.querySelector(".menu");
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    menu.classList.toggle("active");
+}
+
+const link = document.querySelectorAll(".link");
+link.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu(){
+    hamburger.classList.remove("active");
+    menu.classList.remove("active");
+}
+
+var d = new Date();
+var weekday = new Array(7);
+weekday[0] = "Sunday";
+weekday[1] = "Monday";
+weekday[2] = "Tuesday";
+weekday[3] = "Wednesday";
+weekday[4] = "Thursday";
+weekday[5] = "Friday";
+weekday[6] = "Saturday";
+
+var n = weekday[d.getDay()];
+document.querySelector("#currentDayofWeek").innerHTML = n;
+
+var n = d.getDate();
+document.querySelector("#currentDay").innerHTML = n;
+
+var month = new Array();
+month[0] = "January";
+month[1] = "February";
+month[2] = "March";
+month[3] = "April";
+month[4] = "May";
+month[5] = "June";
+month[6] = "July";
+month[7] = "August";
+month[8] = "September";
+month[9] = "October";
+month[10] = "November";
+month[11] = "December";
+
+var n = month[d.getMonth()];
+document.querySelector("#currentMonth").innerHTML = n;
+
+var n = d.getFullYear();
+document.querySelector("#currentYear").innerHTML = n;
+
+//Gallery
+const imagesToLoad = document.querySelectorAll('img[data-src]');
+
+const loadImages = (image) => {
+    image.setAttribute('src', image.getAttribute('data-src'));
+    image.onload = () => {
+        image.removeAttribute('data-src');
+    };
+};
+
+const imgOptions = {
+    rootMargin: '0px 0px 50px 0px',
+    threshold: 1
+};
+
+if ('IntersectionObserver' in window) {
+    const imgObserver = new IntersectionObserver(items => {
+        items.forEach(item => {
+            if (item.isIntersecting) {
+                loadImages(item.target);
+                imgObserver.unobserve(item.target);
+            }
+        });
+    }, imgOptions);
+
+    imagesToLoad.forEach((img) => {
+        imgObserver.observe(img);
+    });
+} else {
+    imagesToLoad.forEach(img => {
+        loadImages(img);
+    });
+}
+
+//Last visited in days.
+window.onload = function() {
+    function betweenVisits(first, second) {
+        const day = 1000 * 60 * 60 * 24;
+        const diffTime = first - second;
+        const diffDays = Math.round(diffTime / day);
+        return diffDays;
+    }
+    if (typeof(Storage) != "undefined") {
+        let newDate = new Date();
+        currentVisit = newDate.getTime();
+
+        if (typeof localStorage.getItem("visited") == typeof "string") {
+            document.querySelector("#visited").innerText = betweenVisits(currentVisit, localStorage.getItem("visited"));
+            let previousVisit = currentVisit;
+            localStorage.setItem("visited", previousVisit);
+        }
+        else {
+            let previousVisit = currentVisit;
+            localStorage.setItem("visited", previousVisit);
+        }
+    }
+}
+
+//JSON
+fetch("json/bc.json")
+  .then(function (response) {
+    return response;
+  })
+  .then(function (jsonObject) {
+    const bc = jsonObject['bc'];
+     {
+        let card = document.createElement('section');
+        let name = document.createElement('h2');
+        let img = document.createElement('img');
+        let phone = document.createElement('p');
+        let url = document.createElement('p');
+
+        name.textContent = `${bc.name}`;
+
+        img.setAttribute('src', `images/${bc.logo}`);
+        img.setAttribute('alt', `${bc.name} img`);
+
+        phone.textContent = `${bc.phone}`;
+        url.textContent = `${bc.url}`;
+
+        card.appendChild(name);
+        card.appendChild(img);
+        card.appendChild(phone);
+        card.appendChild(url);
+
+        document.querySelector('div.places').appendChild(card);
+    }
+  });
---
+++ .history/finalproject/js/weatherapi_20210721170822.js
@@ -0,0 +1,19 @@
+let cityid = '5870133';
+let appID = '4288c843ab0418aa9169c7a83367910c';
+const apiurl = `https://api.openweathermap.org/data/2.5/weather?id=${cityid}&appid=${appID}&units=imperial`;
+
+fetch(apiurl)
+.then((response) => response.json())
+.then((jsObject) => {
+    const temp = document.querySelector('#temp');
+    temp.textContent = jsObject.main.temp.toFixed(0);
+    const humidity = document.querySelector('#humidity');
+    humidity.textContent = jsObject.main.humidity.toFixed(0);
+    const desc = jsObject.weather[0].description;
+    currently.innerHTML = desc;
+
+    const imagesrc = `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
+    document.getElementById('imagesrc').textContent = imagesrc;
+    document.getElementById('icon').setAttribute('src', imagesrc);
+    document.getElementById('icon').setAttribute('alt', desc.toUpperCase());
+});
